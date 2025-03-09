198cb50f86f07668560540cc80db9d705b73ffd1
haris <harishammadpak1@gmail.com>
2025-03-09 20:59:30 +1400

    commit 7th

---
+++ index.js
@@ -0,0 +1,30 @@
+require('dotenv').config()
+const cors = require('cors')
+const express = require('express');
+const router = require('./server/router/router')
+const adminRouter = require('./server/router/AdminRoute')
+const ConnectDb = require('./server/utils/db')
+const corsOption={
+    origin: 'https://todu-front.vercel.app', 
+    methods: 'GET,POST,DELETE,PATCH,PUT,HEAD',
+    credentials:true
+}
+const app = express();
+app.use(cors(corsOption))
+app.use(express.json())
+app.use('/api',router)
+app.use('/api',adminRouter)
+
+
+
+
+
+
+
+const PORT = 3000;
+ConnectDb().then(()=>{
+app.listen(PORT,()=>{
+console.log('server running at this port',PORT);
+}) })
+
+module.exports = app;
